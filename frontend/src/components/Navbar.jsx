af058e8634d1094567834a644a98ddc873edf135
Tejendrasinh Sisodia <tejendrasinh51@gmail.com>
2025-06-19 02:16:55 +0530

    Desktop Nav Ready

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import {assets} from '../assets/assets'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='flex justify-between items-center py-5 font-medium'>
       
+    <NavLink to='/'>
+        <img src={assets.logo} className='w-36 cursor-pointer' alt="logo" />
+    </NavLink>
+
+    <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+
+        <NavLink to='/' className='flex flex-col items-center gap-1'>
+            <p>HOME</p>
+            <hr className='w-1/2 border-none h-[1.5px] bg-gray-700 hidden ' />
+        </NavLink>
+
+        <NavLink to='/collection' className='flex flex-col items-center gap-1'>
+            <p>COLLECTION</p>
+            <hr className='w-1/2 border-none h-[1.5px] bg-gray-700 hidden ' />
+        </NavLink>
+
+        <NavLink to='/about' className='flex flex-col items-center gap-1'>
+            <p>ABOUT</p>
+            <hr className='w-1/2 border-none h-[1.5px] bg-gray-700 hidden ' />
+        </NavLink>
+
+        <NavLink to='/contact' className='flex flex-col items-center gap-1'>
+            <p>CONTACT</p>
+            <hr className='w-1/2 border-none h-[1.5px] bg-gray-700 hidden ' />
+        </NavLink>
+
+    </ul>
+
+    <div className='flex items-center gap-5'>
+        <img src={assets.search_icon} className='w-5 cursor-pointer' alt="search" />
+
+        <div className='group relative'>
+        <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="profile" />
+            <div className='group-hover:block hidden absolute dropdown-menu top-0 pt-4'>
+                <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 shadow-lg rounded text-gray-500'>
+                    <p className='cursor-pointer hover:text-black'>My Profile</p>
+                    <p className='cursor-pointer hover:text-black'>Orders</p>
+                    <p className='cursor-pointer hover:text-black'>Logout</p>
+                </div>
+            </div>
+        </div>
+
+        <Link to='/cart' className='relative'>
+            <img src={assets.cart_icon} className='w-5 min-w-5' alt="cart" />   
+            <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px] '>10</p>
+        </Link>
+
+    </div>
+        
     </div>
   )
 }
