3c287a4d54549f2293ce2560b4a664eff17430d6
Tejendrasinh Sisodia <tejendrasinh51@gmail.com>
2025-06-19 00:46:05 +0530

    routes add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Navbar from './components/Navbar'
+
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] xl:px-[15vw] 2xl:px-[25vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/orders' element={<Orders/>} />
+        <Route path='/placeorder' element={<PlaceOrder/>} />
+        <Route path='/product' element={<Product/>} />
+      </Routes>
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as BrowswerRouter } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowswerRouter>
     <App />
-  </StrictMode>,
+  </BrowswerRouter>
 )
