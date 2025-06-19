ccc6d08e6093de5b0a8597a1db77232e346f5b6a
Tejendrasinh Sisodia <tejendrasinh51@gmail.com>
2025-06-20 01:57:38 +0530

    Hero Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Navbar/>
+      
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
